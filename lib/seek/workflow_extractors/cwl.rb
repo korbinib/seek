2476cd75d9cc09a27abbc4840597d0f0fac2cc4c
Finn Bacall <finn.bacall@manchester.ac.uk>
2022-05-20 14:33:37 +0100

    Fixed SVG/DOT generation for CWL diagrams with quotes
    # Conflicts:
    #	lib/seek/workflow_extractors/cwl.rb

--- lib/seek/workflow_extractors/cwl.rb
+++ lib/seek/workflow_extractors/cwl.rb
@@ -1,3 +1,4 @@
+require 'open4'
 require 'rest-client'
 
 module Seek
@@ -27,8 +28,20 @@ module Seek
           wf = WorkflowInternals::Structure.new(metadata[:internals])
           Seek::WorkflowExtractors::CwlDotGenerator.new(f).write_graph(wf)
           f.rewind
-          `cat #{f.path} | dot -Tsvg`
+          out = ''
+          err = ''
+          Open4.open4('dot -Tsvg') do |pid, stdin, stdout, stderr|
+            stdin.puts(f.read)
+            stdin.close
+            out = stdout.read
+            err = stderr.read
+            stdout.close
+            stderr.close
+          end
+          Rails.logger.error(err) if err.length > 0
+          out
         rescue StandardError => e
+          Rails.logger.error(e)
           nil
         end
       end
@@ -51,8 +64,16 @@ module Seek
           path = f.path
         end
 
-        packed_cwl_string = `cwltool --skip-schemas --quiet --enable-dev --non-strict --pack #{path}`
-        if $?.success?
+        packed_cwl_string = ''
+        status = Open4.popen4("cwltool --skip-schemas --quiet --enable-dev --non-strict --pack #{path}") do |_pid, _stdin, stdout, stderr|
+          while (line = stdout.gets) != nil
+            packed_cwl_string << line
+          end
+          stdout.close
+          stderr.close
+        end
+
+        if status.success?
           cwl_string = packed_cwl_string
         else
           cwl_string ||= @io.read
